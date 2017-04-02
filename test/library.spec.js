e1ef610ec3359af99567baadccf020fcb73f9a62
gravy <mark.eades@gmail.com>
2017-04-02 14:46:59 -0700

    Remove commented code and import

--- test/library.spec.js
+++ test/library.spec.js
@@ -1,5 +1,3 @@
-import lib from '../lib/Library';
-
 const chai = require('chai');
 const Library = require('../lib/Library.js');
 
@@ -10,7 +8,7 @@ const expect = chai.expect;
 describe('Given an instance of my library',  () => {
   describe('when I need the name', () => {
     it('should return the name', () => {
-      expect(lib.name).to.be.equal('Library');
+      expect(Library.name).to.be.equal('Library');
     });
   });
 });
@@ -18,7 +16,7 @@ describe('Given an instance of my library',  () => {
 let example;
 describe('Given an instance of example', () => {
   before(() => {
-    example = lib.example([1,2,3]);
+    example = Library.example([1,2,3]);
   });
   describe('when I access the items of the example', () => {
     it('should return the first item', () => {
